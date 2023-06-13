273b0097e60b7a24dc6cdcb207e4ecbc5ad5ba30
unknown <ichomatta20@gmail.com>
2023-06-13 19:35:37 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  $(".btn-next").on("click", function () {
+    var currImg = $(".active");
+    var nextImg = currImg.next();
+
+    if (nextImg.length) {
+      currImg.removeClass("active").css("z-index", -10);
+      nextImg.addClass("active").css("z-index", 10);
+    } else {
+      currImg.removeClass("active").css("z-index", -10);
+      $(".image-slider #img:first-child").addClass("active").css("z-index", 10);
+    }
+  });
+
+  $(".btn-prev").on("click", function () {
+    var currImg = $(".active");
+    var prevImg = currImg.prev();
+
+    if (prevImg.length) {
+      currImg.removeClass("active").css("z-index", -10);
+      prevImg.addClass("active").css("z-index", 10);
+    } else {
+      currImg.removeClass("active").css("z-index", -10);
+      $(".image-slider #img:last-child").addClass("active").css("z-index", 10);
+    }
+  });
+});
+
+function dropdownMenu() {
+  document.querySelector(".dropdown-list").classList.toggle("show");
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches(".dropBtn")) {
+    var dropdownList = document.querySelector(".dropdown-list");
+    if (dropdownList.classList.contains("show")) {
+      dropdownList.classList.remove("show");
+    }
+  }
+};
+
+function goScreenshot() {
+  window.location.href = "../html/screenshot.html";
+}
